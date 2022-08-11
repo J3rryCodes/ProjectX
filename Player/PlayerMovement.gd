afc07a1adeb60dd20f22acf5c087ef1e4f18dbab
j3rry <jerinjohnykutty002@gmail.com>
2022-08-11 08:28:56 +0530

    and sprint,walk

--- Player/PlayerMovement.gd
+++ Player/PlayerMovement.gd
@@ -1,21 +1,23 @@
 extends KinematicBody
 
-export var speed = 14
+export var sprint_speed = 18
+export var walk_speed = 12
 
 var velocity = Vector3.ZERO
-var jump_acc = 15
+var jump_acc = 18
 var gravity = 0.98
+var is_sprint = false
 
 onready var head = $Head
 
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
+	
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	var head_basics = head.global_transform.basis
+	
+	is_sprint = Input.is_action_pressed("sprint")
 	
 	if Input.is_action_pressed("fwd"):
 		direction -= head_basics.z
@@ -30,12 +32,18 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		direction.y = 1
 	elif is_on_floor():
-		direction.y = -0.1
+		direction.y = -0.01
 		
 	direction = direction.normalized()
 		
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity.x = direction.x *  walk_speed
+	
+	# Shift key pressed and moving forword
+	if is_sprint and direction.z < 0:
+		velocity.z = direction.z * sprint_speed
+	else:
+		velocity.z = direction.z * walk_speed
+	
 	velocity.y += direction.y * jump_acc
 	velocity.y -= gravity
 	
